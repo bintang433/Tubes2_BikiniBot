1b060063fd57fb977c33ae6e44223ea45558f782
bintang433 <13521003@std.stei.itb.ac.id>
2023-03-17 23:36:11 +0700

    Add project files.

---
+++ src/Maze.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeSolver.src
+{
+    internal class Maze
+    {
+        private int length;
+        private int width;
+        private Node[,] grid;
+        private int[,] awal;
+        private int[,] akhir;
+        private List<List<char>> peta = new List<List<char>>();
+
+        public Maze(Node[,] grid, int[,] awal, int[,] akhir)
+        {
+            this.grid = grid;
+            this.awal = awal;
+            this.akhir = akhir;
+        }
+        public int Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+        public int Width
+        { 
+            get { return width; } 
+            set { width = value; }
+        }
+        public void setGrid(int row, int col, Node node)
+        {
+            grid[row, col] = node;
+        }
+
+        public void createMap(String filename)
+        {
+            String filePath = "../config/" + filename;
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line = reader.ReadLine(); // Read the first line of the file
+                this.peta.Add(new List<char>(line.Split(' '))); // Add the first line to the list of lists
+                while ((line = reader.ReadLine()) != null)
+                {
+                    this.peta.Add(new List<char>(line.Split(' '))); // Add each subsequent line to the list of lists
+                }
+                this.length = peta.Count;
+                this.width = peta[0].Count;
+            }
+        }
+
+        public void printMap()
+        {
+            foreach (List<char> row in peta) 
+            {
+                foreach(char c in row)
+                {
+                    Console.Write(c);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void solveBFS(int time)
+
+        {
+
+        }
+
+        public void solveDFS(int time)
+        {
+
+        }
+    }
+}
---
+++ src/Node.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace MazeSolver.src
+{
+    internal class Node
+    {
+        List<Node> children;
+        bool treasure;
+        bool mrCrab;
+        public Node(bool isTreasure, bool isMrCrab)
+        {
+            children = new List<Node>();
+            treasure = isTreasure;
+            mrCrab = isMrCrab;
+        }
+        public bool Treasure
+        {
+            get
+            {
+                return treasure;
+            }
+            set
+            {
+                treasure = value;
+            }
+        }
+        public bool MrCrab
+        {
+            get
+            {
+                return mrCrab;
+            }
+            set
+            {
+                mrCrab = value;
+            }
+        }
+        public List<Node> Children
+        {
+            get
+            {
+                return children;
+            }
+        }
+        public void addChildren(bool isTreasure, bool isMrCrab)
+        {
+            Node child = new Node(isTreasure, isMrCrab);
+            children.Add(child);
+        }
+
+        public bool dfs()
+        {
+            if (Treasure && MrCrab)
+            {
+                return true;
+            }
+            else
+            {
+                bool found = false;
+                foreach (Node child in Children)
+                {
+                    found = found || child.dfs();
+                    if (found)
+                        break;
+                }
+                return found;
+            }
+        }
+
+        public bool bfs()
+        {
+            Queue<Node> childQueue = new Queue<Node>();
+            if (Treasure)
+            {
+                return true;
+            }
+            foreach (Node child in Children)
+            {
+                childQueue.Enqueue(child);
+            }
+            while (childQueue.Count != 0)
+            {
+                Node checkChild = childQueue.Dequeue();
+                if (checkChild.Treasure)
+                {
+                    return true;
+                }
+                foreach (Node grandchild in checkChild.Children)
+                {
+                    childQueue.Enqueue(grandchild);
+                }
+            }
+            return false;
+        }
+    }
+}
